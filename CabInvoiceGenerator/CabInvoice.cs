24fc329c1d219a254cb8e7a9639dd85c0dfdfa85
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-04-21 15:15:08 +0530

    [sanjana] Add :Get Multiple Ride

--- CabInvoiceGenerator/CabInvoice.cs
+++ CabInvoiceGenerator/CabInvoice.cs
@@ -6,11 +6,21 @@
         private static readonly double COST_PER_MIN = 1.0;
         private static readonly double MIN_FARE = 5.0;
         private double cabfare = 0.0;
+        
         public double CalulateFare(double distance, double time)
         {
             this.cabfare = (distance * COST_PER_KM) + (time * COST_PER_MIN);
             return Math.Max(this.cabfare, MIN_FARE);
 
+        }
+        public double GetMultipleRideFare(Ride[] rides)
+        {
+            double totalRideFare = 0.0;
+            foreach (var ride in rides)
+            {
+                totalRideFare += this.CalulateFare(ride.rideDistance, ride.rideTime);
+            }
+            return totalRideFare/rides.Length;
         }
     }
 }
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -11,5 +11,14 @@ namespace TestProject1
             double totalFare = cabInvoice.CalulateFare(3.0, 5.0);
             Assert.AreEqual(35.0, totalFare);
         }
+        [Test]
+        public void Test2()
+        {
+            Ride[] ride = { new Ride(3.0, 5.0), new Ride(2.0, 5.0) };
+            CabInvoice cabInvoice = new CabInvoice();
+            double totalFare = cabInvoice.GetMultipleRideFare(ride);
+            Assert.AreEqual(30.0, totalFare);
+
+        }
     }
 }
